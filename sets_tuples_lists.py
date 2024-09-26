a36fe83774e3a637309e2c3bacb2b023ec64d390
Suarez <vssuarez1@cps.k12.il.us>
2024-09-26 08:51:02 -0500

    list car

--- sets_tuples_lists.py
+++ sets_tuples_lists.py
@@ -17,18 +17,52 @@ fruits[0] = "pineapple" #i can reassign values with this
 print(fruits)
 
 fruits.append("pineapple") #add elements to the list
-fruits.remove("apple") #removes elements 
+#fruits.remove("apple") #removes elements 
 fruits.insert(0, "pineapple") #
 fruits.sort() #sorts the list
 fruits.reverse() #reverses the list
 fruits.clear() #clears the elements in the list 
-
+#print(fruits.index("coconut"))
 
 #for fruit in fruits:
 #print(fruit)
 
 
 
+cars = ["bmw","maserati","audi","mercedes","ferrari"]
+print(f"these are the list of {cars}")
+print(f"the first car is {cars[0]}")
+
+
+#changing the value of the list
+cars[0] = "toyota"
+print(f"the first car is {cars[0]}")
+
+print(f"the last car is {cars[-1]}")
+cars[-1] = "lamborghini"
+print(f"the last car is {cars[-1]}")
+
+
+#adding a new value to the list
+cars.append("bugatti")
+print(cars)
+cars.remove("maserati")
+print(cars)
+
+
+#looping thru the list
+#otherwise called iterating thru the list 
+for car in cars: 
+    #print(len(car))
+    #print(car)
+    carRequest = input("add a car please")
+    cars.append(carRequest)
+    print(cars)
+    print(len(cars))
+    print(cars.upper())
+    print(cars)
+    if len(cars) > 10:
+        break
 
 
 
