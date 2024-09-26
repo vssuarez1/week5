90a2eadcd977472ebf7a470e124e00b46325e875
Suarez <vssuarez1@cps.k12.il.us>
2024-09-26 09:34:34 -0500

    friends

--- sets_tuples_lists.py
+++ sets_tuples_lists.py
@@ -3,70 +3,79 @@
 #set = {} unordered and immutable, but Add/Remove OK. NO duplicates
 #table = () ordered and unchangeable. Duplicates OK. FASTER 
 
-fruits = ["apple", "orange", "banana","coconut","kiwi","strawberry"]
-print(fruits[0])
+# fruits = ["apple", "orange", "banana","coconut","kiwi","strawberry"]
+# print(fruits[0])
 
 
 
-#print(dir(fruits))
-# print(len(fruits))
-print(len(fruits)) 
-print("pinaeapple" in fruits) #checks for pineapple in list: boolean value if it is true or not 
+# #print(dir(fruits))
+# # print(len(fruits))
+# print(len(fruits)) 
+# print("pinaeapple" in fruits) #checks for pineapple in list: boolean value if it is true or not 
 
-fruits[0] = "pineapple" #i can reassign values with this 
-print(fruits)
+# fruits[0] = "pineapple" #i can reassign values with this 
+# print(fruits)
 
-fruits.append("pineapple") #add elements to the list
-#fruits.remove("apple") #removes elements 
-fruits.insert(0, "pineapple") #
-fruits.sort() #sorts the list
-fruits.reverse() #reverses the list
-fruits.clear() #clears the elements in the list 
-#print(fruits.index("coconut"))
+# fruits.append("pineapple") #add elements to the list
+# #fruits.remove("apple") #removes elements 
+# fruits.insert(0, "pineapple") #
+# fruits.sort() #sorts the list
+# fruits.reverse() #reverses the list
+# fruits.clear() #clears the elements in the list 
+# #print(fruits.index("coconut"))
 
-#for fruit in fruits:
-#print(fruit)
-
-
-
-cars = ["bmw","maserati","audi","mercedes","ferrari"]
-print(f"these are the list of {cars}")
-print(f"the first car is {cars[0]}")
+# #for fruit in fruits:
+# #print(fruit)
 
 
-#changing the value of the list
-cars[0] = "toyota"
-print(f"the first car is {cars[0]}")
 
-print(f"the last car is {cars[-1]}")
-cars[-1] = "lamborghini"
-print(f"the last car is {cars[-1]}")
+# cars = ["bmw","maserati","audi","mercedes","ferrari"]
+# print(f"these are the list of {cars}")
+# print(f"the first car is {cars[0]}")
 
 
-#adding a new value to the list
-cars.append("bugatti")
-print(cars)
-cars.remove("maserati")
-print(cars)
+# #changing the value of the list
+# cars[0] = "toyota"
+# print(f"the first car is {cars[0]}")
+
+# print(f"the last car is {cars[-1]}")
+# cars[-1] = "lamborghini"
+# print(f"the last car is {cars[-1]}")
+
+
+# #adding a new value to the list
+# cars.append("bugatti")
+# print(cars)
+# cars.remove("maserati")
+# print(cars)
 
 
 #looping thru the list
 #otherwise called iterating thru the list 
-for car in cars: 
+#for car in cars: 
     #print(len(car))
     #print(car)
-    carRequest = input("add a car please")
-    cars.append(carRequest)
-    print(cars)
-    print(len(cars))
-    print(cars.upper())
-    print(cars)
-    if len(cars) > 10:
-        break
+    #carRequest = input("add a car please")
+    #cars.append(carRequest)
+    #print(cars)
+    #print(len(cars))
+    #print(cars.upper())
+    #print(cars)
+   # if len(cars) > 10:
+       # break
 
-
-
-
+friends = []
+addFriend = input("add a friend please: ")
+friends.append(addFriend)
+print(friends) 
+for friend in friends:
+    addFriend = input("add a friend please: ")
+    friends.append(addFriend)
+    print(friends)
+    if len(friends) > 6: 
+        break 
+friends.remove("idk")
+print(friends)
 
 
 
